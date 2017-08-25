b21f69c36cace9075d0df4eba646f195c968ab3a
Alexis DeRuyter <email": null>
2017-08-24 17:39:27 -0700

    Add logout

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -5,7 +5,7 @@ end
 
 post '/login' do
   user_params = params[:user]
-  @user = User.authenticate(user_params[:email], user_params[:password])
+  @user = User.authenticate(user_params[:username], user_params[:password])
   if @user
     session[:user_id] = @user.id
     redirect :'/profile'
