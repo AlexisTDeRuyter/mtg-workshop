38664da9e8f46b230dee655108405ea9097fd890
Alexis DeRuyter <email": null>
2017-08-24 20:45:50 -0700

    Add card, card_type, migrations and models, restyle buttons in nav bar

---
+++ db/migrate/20170824192848_create_cards.rb
@@ -0,0 +1,11 @@
+class CreateCards < ActiveRecord::Migration[5.1]
+  def change
+    create_table :cards do |t|
+      t.integer :card_id, null: false
+      t.integer :user_id, null: false
+      t.integer :quantity, null: false, default: 1
+
+      t.timestamps
+    end
+  end
+end
